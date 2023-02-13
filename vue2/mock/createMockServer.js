c91c533ec4f1e79a1319a6b90ea84812a2f9cf12
chenzl <chenzhangle@anicert.cn>
2023-02-13 09:05:00 +0800

    tag：构建vue2.x+vite3版本的后台模板

--- vue2/mock/createMockServer.js
+++
@@ -1,9 +0,0 @@
-import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer';
-
-// 逐一导入您的 mock 文件
-// 可以使用 import.meta.glob功能来进行全部导入
-import userModule from './user';
-
-export function setupProdMockServer() {
-  createProdMockServer([...userModule]);
-}
